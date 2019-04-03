e86257bb17deff179df9c5de0de49ded16e5935e
Charlene Sheehan <31358580+charlene-sheehan@users.noreply.github.com>
2019-04-03 12:37:43 +0200

    Part 7 - example module
    The example module that accompanies Part 7

---
+++ python-intro-part-7-examplemodule.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 15 22:33:44 2019
+
+@author: ccsheehan
+Intro to Python course. Part 7
+Lesson numbering began after the installation lesson.
+Covers how to make your own modules
+"""
+#Lesson 20 Module Making
+
+def exampleFunc(data):
+    print(data)
+    
+exampleFunc('this is a test')
